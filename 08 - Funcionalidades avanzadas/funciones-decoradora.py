ef852f2621c763820a2e384f06e2db15095ad529
AndresDamelio <andresdamelio24@gmail.com>
2019-04-28 21:56:18 -0400

    funciones decoradoras ejemplo

--- 08 - Funcionalidades avanzadas/funciones-decoradora.py
+++ 08 - Funcionalidades avanzadas/funciones-decoradora.py
@@ -1,11 +1,10 @@
-
 """ 
 ==============================================================================================
-                        FUNCIÓN LOCALS Y GLOBALS
+                        FUNCION LOCALS Y GLOBALS
 ==============================================================================================
  """
  
-# En python existen dos funciones donde se pueden encontrar los elementos globles y locales en un programa python, estos elementos pueden ser clases funciones, variables, se pueden usar llamando globals() o locals(), estas funciones devuelven un diccionario
+ # En python existen dos funciones donde se pueden encontrar los elementos globles y locales en un programa python, estos elementos pueden ser clases funciones, variables, se pueden usar llamando globals() o locals(), estas funciones devuelven un diccionario
  
 #print(locals())
 #print(globals())
@@ -16,28 +15,40 @@
 ==============================================================================================
  """
  
- # Es una función que envuelve la ejecución de otra función, y permite extender su comportamiento, estan pensadas para reutilizarlas gracias a una sintaxis de ejecución mucho mas simple
+ # Es una funcion que envuelve la ejecucion de otra funcion, y permite extender su comportamiento, estan pensadas para reutilizarlas gracias a una sintaxis de ejecucion mucho mas simple
  
  # ejemplo de como usar una funcion decoradora en cualquier funcion, el ejemplo es de operaciones basicas de matematica, sencillas sin validaciones, suponiendo que los argumentos a recibir son validos
 
 
-# Como crear una función decoradora
+ 
+# Como crear una funcion decoradora
 def operaciones_decorador( funcion ):
     def decorar( a,b ):
-        print("Se ejecutará la {} de {} y {}".format(funcion.__name__, a, b))
+        print("Se ejecutara la {} de {} y {}".format(funcion.__name__, a, b))
         funcion(a,b)
         print("El resultado de la {} de {} y {} es {}".format(funcion.__name__, a, b, a+b))
     return decorar
 
-# En este caso se esta creando una función principal, que tiene una función interna que sera la encargada de ejecutar la función recibida como argumento y monitorizar la ejecución de esta misma, para agregar la función decoradora a una función solo basta con agregar @nombre_funcion_decoradora
+# En este caso se esta creando una funcion principal, que tiene una funcion interna que sera la encargada de ejecutar la funcion recibida como argumento y monitorizar la ejecucion de esta misma, para agregar la funcion decoradora a una funcion solo basta con agregar @nombre_funcion_decoradora
 
+@operaciones_decorador
 def suma(a,b):
-    print( a + b )
+    print( "{} + {}".format(a,b) )
+
+@operaciones_decorador
 def resta(a,b):
-    print( a - b )
+    print( "{} - {}".format(a,b) )
+
+@operaciones_decorador
 def multiplicacion(a,b):
-    print( a * b )
+    print( "{} * {}".format(a,b) )
+
+@operaciones_decorador
 def division(a,b):
-    print( a / b )
-# Como crear una funcion decoradora
+    print( "{} / {}".format(a,b) )
+
+suma(3,2)
+resta(10,5)
+multiplicacion(3,10)
+division(4,3)
 
